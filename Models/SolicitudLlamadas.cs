f207e1b43d93cf41c012d0f281791dfa23b4c215
juanca14112020 <juancahq27@gmail.com>
2026-06-02 09:48:22 -0500

    feat: integración de llamadas con Twilio en .NET

---
+++ Models/SolicitudLlamadas.cs
@@ -0,0 +1,8 @@
+namespace NiquiBackedn.Models
+{
+    public class SolicitudLlamada
+    {
+        public string TelefonoCliente { get; set; } = string.Empty;
+        public string NombreCliente { get; set; } = string.Empty;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,37 @@
+var builder = WebApplication.CreateBuilder(args);
+
+//Configurar servicios del sistema
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermitirVue", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+            .AllowAnyHeader()
+            .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build();
+
+// Activar documentación visual en entorno de desarrollo
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseCors("PermitirVue");
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
+
